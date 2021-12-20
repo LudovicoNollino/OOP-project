354827d3b5e73001f0b3a57820a6dcd0b428ecca
smnrcc <95432231+smnrcc@users.noreply.github.com>
2021-12-20 23:10:03 +0100

    Add files via upload

---
+++ controller/src/test/java/it/univpm/TweetHashtagStats/controller/ControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.TweetHashtagStats.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
