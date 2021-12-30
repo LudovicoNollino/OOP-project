4eb7c9c199eb3b7970f4b749e8f830f6e495ba21
Ludovico Nollino <s1093732@studenti.univpm.it>
2021-12-30 12:45:34 +0100

    Prova

---
+++ TwitterHashtag/src/main/java/it/univpm/TwitterHashtag/TwitterHashtagApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.TwitterHashtag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterHashtagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterHashtagApplication.class, args);
+	}
+
+}
---
+++ TwitterHashtag/src/test/java/it/univpm/TwitterHashtag/TwitterHashtagApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.TwitterHashtag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterHashtagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
